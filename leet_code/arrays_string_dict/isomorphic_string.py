b9eecaf874b4d85852308b04252394a2c002621e
vishruti-patel <vi94.patel@gmail.com>
2025-06-19 01:02:55 -0700

    vishruti-patel-06-18-2025

---
+++ leet_code/arrays_string_dict/isomorphic_string.py
@@ -0,0 +1,16 @@
+def isomorphic(s, t):
+        map1 = []
+        map2 = []
+
+        for i in s:
+            map1.append(s.index(i))
+        for i in t:
+            map2.append(t.index(i))
+        if map1 == map2:
+            return True
+
+        return False
+
+if __name__ == "__main__":
+     print(isomorphic('egg', 'add'))
+    
