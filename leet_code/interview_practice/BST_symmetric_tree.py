17c1fd60cbda585a13729bc46c528a6bcf3f93a6
vishruti-patel <vi94.patel@gmail.com>
2025-09-14 15:15:53 -0700

    trees-practice

---
+++ leet_code/interview_practice/BST_symmetric_tree.py
@@ -0,0 +1,19 @@
+class TreeNode:
+    def __init__(self, val, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+class Solution:
+    def isSymmetric(self, root):
+        def dfs(left_root, right_root):
+            if not left_root and not right_root:
+                return True
+            
+            if not left_root or not right_root:
+                return False
+            
+            return (left_root.val == right_root.val  and dfs(left_root.left, right_root.right) and dfs(left_root.right, right_root.left))
+
+
+        return dfs(root.left, root.right)
